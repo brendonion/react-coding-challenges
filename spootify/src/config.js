e739ec2e72d5fd38473c0adb7c51636afb5755c3
Brendan Walker <brenny_walker@hotmail.com>
2021-09-08 15:34:51 -0700

    Adds spotify clientId and clientSecret to config.js (TODO: remove). Adds utils.js folder with all necessary api calls. Makes parallel api calls from within Discover component.

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '11a33f993c0a4489ad31dddede44e7e6',
+    clientSecret: '2d75249051054cab81383ac06a30272f'
   }
 }
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
+import { authorize, fetchNewReleases, fetchPlaylists, fetchCategories } from '../../../utils';
 import '../styles/_discover.scss';
 
 export default class Discover extends Component {
@@ -9,8 +10,19 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
+      isLoading: true,
     };
+  }
+
+  async componentDidMount() {
+    const { access_token } = await authorize();
+    const [ newReleases, playlists, categories ] = await Promise.all([
+      fetchNewReleases(access_token),
+      fetchPlaylists(access_token),
+      fetchCategories(access_token),
+    ]);
+    this.setState({ newReleases, playlists, categories, isLoading: false });
   }
 
   render() {
